9f42950a302ddf72d795fb64cac0b5053e333a41
Rui Peres <sauron.is.alive@gmail.com>
2013-12-21 18:39:12 +0000

    Added the NSObject+Dealloc category

---
+++ DemoProject/DemoProject/NSObject+Dealloc/NSObject+Dealloc.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Dealloc.h
+//  DemoProject
+//
+//  Created by Rui Peres on 21/12/2013.
+//  Copyright (c) 2013 Aphely. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Dealloc)
+
+@end
---
+++ DemoProject/DemoProject/NSObject+Dealloc/NSObject+Dealloc.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Dealloc.m
+//  DemoProject
+//
+//  Created by Rui Peres on 21/12/2013.
+//  Copyright (c) 2013 Aphely. All rights reserved.
+//
+
+#import "NSObject+Dealloc.h"
+
+@implementation NSObject (Dealloc)
+
+@end
